693a08cd400c4cfd0d89e377fcbc111c80a3cf70
Szymon Oponczewski <szymon_o@mac.com>
2023-04-05 23:19:30 +0200

    code update task 1 & 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const listEl = `
+  <li>
+    <a class="gallery__item" href="${image.original}">
+      <img class="gallery__image"
+        src="${image.preview}"
+        alt="${image.description}"
+         />
+    </a>
+    </li>
+  `;
+  galleryEl.insertAdjacentHTML("beforeend", listEl);
+});
+
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+const lightbox = new SimpleLightbox(".gallery li a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+const throttle = require("lodash.throttle");
+const iframe = document.querySelector("iframe");
+const player = new Vimeo.Player(iframe);
 
+function timeStamp(data) {
+  localStorage.setItem("videoplayer-current-time", data.seconds);
+}
+
+player.on("timeupdate", throttle(timeStamp, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
